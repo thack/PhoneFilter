1021c35e19c179e4b21f95f734621358ffeade8d
Terence Carroll <git@thack.com>
2014-08-03 21:42:35 -0700

    Fix null input.

--- phone.js
+++ phone.js
@@ -2,6 +2,7 @@ angular.module('filter', []);
 
 angular.module('filter').filter('phonenumber', function() {
     return function(input) {
+        input = input || '';
         var digits = input.replace(/^\d/g, '');
         return '(' + digits.substr(0, 3) + ') ' + digits.substr(3, 3) + '-' + digits.substr(6, 4);
     };
