f1f947723082b207b3cb5b5987ed3b94577b2dde
Terence Carroll <git@thack.com>
2014-08-03 21:46:08 -0700

    Greedy match regex.

--- phone.js
+++ phone.js
@@ -3,7 +3,7 @@ angular.module('filter', []);
 angular.module('filter').filter('phonenumber', function() {
     return function(input) {
         input = input || '';
-        var digits = input.replace(/^\d/g, '');
+        var digits = input.replace(/[^\d]+/g, '');
         return '(' + digits.substr(0, 3) + ') ' + digits.substr(3, 3) + '-' + digits.substr(6, 4);
     };
 });
