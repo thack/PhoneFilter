c05396ded23d59b674bee5273e0aeab379fb322b
Terence Carroll <git@thack.com>
2014-08-03 22:15:35 -0700

    Third attempt.

--- filter.cpp
+++ filter.cpp
@@ -2,16 +2,19 @@
 
 int main(int argc, char *argv[])
 {
-    char *input = "(612)567-9876";
+    char input[255] = "(612)567-9876";
     char digits[255];
+    char *p, *out;
     
-    while (*input != '\0')
+    p = input;
+    out = digits;
+    while (*p != '\0')
     {
-        if (*input >= '0' && *input <= '9')
-            *digits = *input;
-        input++;
+        if (*p >= '0' && *p <= '9')
+            *(out++) = *p;
+        p++;
     }
-    *digits ='\0';
+    *out ='\0';
     
     printf("%s\n%s\n", input, digits);
 }
